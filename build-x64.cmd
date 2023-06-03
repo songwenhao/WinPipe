3168a06d63c6ccf3565246b82698c812be35782a
songwenhao <danjing23615@outlook.com>
2023-06-03 11:12:06 +0800

    1. Modify build step.
    Signed-off-by: songwenhao <danjing23615@outlook.com>

--- build-x64.cmd
+++ build-x64.cmd
@@ -24,9 +24,31 @@ call xcopy RelWithDebInfo\*.lib ..\libs\x64\ /y
 call xcopy RelWithDebInfo\*.pdb ..\libs\x64\ /y
 call xcopy RelWithDebInfo\*.exe ..\libs\x64\ /y
 
+cd ..
+call rmdir /s /q build-StaticRuntime
+call mkdir build-StaticRuntime
+call cmake -S . -B build-StaticRuntime -A x64 -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=ON -DCMAKE_INSTALL_PREFIX:PATH=install
+cd build-StaticRuntime
+call cmake --build . --target INSTALL --config Debug
+call cmake --build . --target INSTALL --config RelWithDebInfo
+call xcopy Debug\*.lib ..\protobuf\libs\StaticRuntime\x64\ /y
+call xcopy Debug\*.pdb ..\protobuf\libs\StaticRuntime\x64\ /y
+call xcopy Debug\*.exe ..\protobuf\libs\StaticRuntime\x64\ /y
+call xcopy RelWithDebInfo\*.lib ..\protobuf\libs\StaticRuntime\x64\ /y
+call xcopy RelWithDebInfo\*.pdb ..\protobuf\libs\StaticRuntime\x64\ /y
+call xcopy RelWithDebInfo\*.exe ..\protobuf\libs\StaticRuntime\x64\ /y
+
 cd %ROOT_PATH%
 call rmdir /s /q build
 call mkdir build
 call cmake -S . -B build -A x64
 call xcopy %ROOT_PATH%\protobuf\libs\x64\*.dll build\Debug\ /y
 call xcopy %ROOT_PATH%\protobuf\libs\x64\*.dll build\RelWithDebInfo\ /y
+
+cd ..
+call rmdir /s /q build-StaticRuntime
+call mkdir build-StaticRuntime
+call cmake -S . -B build-StaticRuntime -A x64 -DMSVC_STATIC_RUNTIME=ON -DBUILD_SHARED_LIBS=OFF
+cd build-StaticRuntime
+call cmake --build . --config Debug
+call cmake --build . --config RelWithDebInfo
--- build-x86.cmd
+++ build-x86.cmd
@@ -23,9 +23,29 @@ call xcopy RelWithDebInfo\*.dll ..\libs\Win32\ /y
 call xcopy RelWithDebInfo\*.lib ..\libs\Win32\ /y
 call xcopy RelWithDebInfo\*.pdb ..\libs\Win32\ /y
 call xcopy RelWithDebInfo\*.exe ..\libs\Win32\ /y
+cd ..
+call rmdir /s /q build-StaticRuntime
+call mkdir build-StaticRuntime
+call cmake -S . -B build-StaticRuntime -A Win32 -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=ON -DCMAKE_INSTALL_PREFIX:PATH=install
+cd build-StaticRuntime
+call cmake --build . --target INSTALL --config Debug
+call cmake --build . --target INSTALL --config RelWithDebInfo
+call xcopy Debug\*.lib ..\protobuf\libs\StaticRuntime\Win32\ /y
+call xcopy Debug\*.pdb ..\protobuf\libs\StaticRuntime\Win32\ /y
+call xcopy Debug\*.exe ..\protobuf\libs\StaticRuntime\Win32\ /y
+call xcopy RelWithDebInfo\*.lib ..\protobuf\libs\StaticRuntime\Win32\ /y
+call xcopy RelWithDebInfo\*.pdb ..\protobuf\libs\StaticRuntime\Win32\ /y
+call xcopy RelWithDebInfo\*.exe ..\protobuf\libs\StaticRuntime\Win32\ /y
 cd %ROOT_PATH%
 call rmdir /s /q build
 call mkdir build
 call cmake -S . -B build -A Win32
 call xcopy %ROOT_PATH%\protobuf\libs\Win32\*.dll build\Debug\ /y
 call xcopy %ROOT_PATH%\protobuf\libs\Win32\*.dll build\RelWithDebInfo\ /y
+cd ..
+call rmdir /s /q build-StaticRuntime
+call mkdir build-StaticRuntime
+call cmake -S . -B build-StaticRuntime -A Win32 -DMSVC_STATIC_RUNTIME=ON -DBUILD_SHARED_LIBS=OFF
+cd build-StaticRuntime
+call cmake --build . --config Debug
+call cmake --build . --config RelWithDebInfo
